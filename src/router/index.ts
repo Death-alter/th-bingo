a133f45d7f333db86ae5eb8380e4e0d6fed3970b
Death <546115696@qq.com>
2025-05-03 00:00:31 +0800

    router改成hash模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import { useRoomStore } from "@/store/RoomStore";
 import { useLocalStore } from "@/store/LocalStore";
 
@@ -28,7 +28,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
